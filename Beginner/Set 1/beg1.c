4ce4f613e4a5c17cb1012156efaf0fa3a5905d7f
Purnima Varshney <purnima2011varshney@gmail.com>
2018-01-14 08:29:10 -0800

    abc
    abc

--- Beginner/Set 1/beg1.c
+++ Beginner/Set 1/beg1.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-	int n;
-	scanf("%i",&n);
-	if(n>0)
+	int m;
+	scanf("%i",&m);
+	if(m>0)
 		printf("Positive");
-	else if(n<0)
+	else if(m<0)
 		printf("Negative");
-	else if(n=0)
+	else if(m=0)
 		printf("Zero");
 	else
 		printf("Invalid Input");
